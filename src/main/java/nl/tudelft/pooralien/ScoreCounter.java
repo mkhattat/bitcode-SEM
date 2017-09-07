8b8de52b082db5baeabbc1d9375720d5469f887d
IvoSchols <Ivoschols97@gmail.com>
2017-09-07 13:19:50 +0200

    Fixed checkstyle errors, currently zero remain

--- src/main/java/nl/tudelft/pooralien/ScoreCounter.java
+++ src/main/java/nl/tudelft/pooralien/ScoreCounter.java
@@ -1,55 +1,58 @@
 package nl.tudelft.pooralien;
 
+/**
+ * ScoreCounter keeps track of the score of the player.
+ * The score should be reset every level.
+ */
 class ScoreCounter {
 
     private int score;
     private int scorePerTile = 1;
 
     /**
-     * Standard initialization
-     * @param score
+     * Standard initialization of the class.
+     * @param score will be the value that the player starts with.
      */
-    void ScoreCounter(int score) {
+    ScoreCounter(int score) {
         this.score = score;
     }
 
     /**
-     * Pre: tilesDestroyed is bigger than 0
-     * Updates the score by multiplying the tilesDestroyed by scorePerTile
-     * @param tilesDestroyed
+     * Pre: tilesDestroyed is bigger than zero.
+     * Updates the score by multiplying the tilesDestroyed by scorePerTile.
+     * @param tilesDestroyed is the amount of tiles the player has destroyed, in one single move.
      */
     public void updateScore(int tilesDestroyed) {
-        if(tilesDestroyed > 0) {
+        if (tilesDestroyed > 0) {
             this.score = this.score + (tilesDestroyed * this.scorePerTile);
         }
     }
 
     /**
-     * Returns the current score of the player
-     * @return
+     * @return Returns the current score of the player.
      */
     public int getScore() {
         return this.score;
     }
 
     /**
-     * Pre: newScore is bigger or equal to 0
-     * Sets score to newScore
-     * @param newScore
+     * Pre: newScore is bigger or equal to zero.
+     * Sets score to newScore.
+     * @param newScore will replace the value of the integer score of the ScoreCounter object.
      */
     public void setScore(int newScore) {
-        if(newScore >= 0) {
+        if (newScore >= 0) {
             this.score = newScore;
         }
     }
 
     /**
-     * Pre: scorePerTile is bigger than 0
-     * Sets scorePerTile to scorePerTile
-     * @param scorePerTile
+     * Pre: scorePerTile is bigger than zero.
+     * Sets scorePerTile to scorePerTile.
+     * @param scorePerTile will set the multiplier with which the destroyed tiles are multiplied.
      */
     public void setScorePerTile(int scorePerTile) {
-        if(scorePerTile > 0) {
+        if (scorePerTile > 0) {
             this.scorePerTile = scorePerTile;
         }
     }
