642c50fe9f4dd71f54de58272e6bc857c7936ca9
Alex van der Linden <alexvanderlinden@me.com>
2017-09-07 12:34:57 +0200

    Initial commit creating EventHandler branch and EventHandler class. Added a window in the launcher class for debugging purpose

---
+++ src/main/java/nl/tudelft/pooralien/EventHandler.java
@@ -0,0 +1,4 @@
+package nl.tudelft.pooralien;
+
+public class EventHandler {
+}
--- src/main/java/nl/tudelft/pooralien/Launcher.java
+++ src/main/java/nl/tudelft/pooralien/Launcher.java
@@ -1,15 +1,20 @@
 package nl.tudelft.pooralien;
 
+
+import javax.swing.JFrame;
+
 /**
  * Created by mostafa on 7-9-17.
  */
-final class Launcher {
+final class Launcher extends JFrame {
 
     /**
      * To prevent checkstyle error.
      */
     private Launcher() {
-
+        setSize(300, 200);
+        setTitle("Simple");
+        setDefaultCloseOperation();
     }
 
     /**
@@ -19,6 +24,8 @@ final class Launcher {
     public static void main(String[] args) {
 
         System.out.println("Hi");
+        Launcher simple = new Launcher();
+        simple.setVisible(true);
 
     }
 }
