b86739bd7ca367b4d82351b3a0f7da5b7a2a4633
Mostafa Khattat <m.khattat@student.tudelft.nl>
2017-09-07 11:20:13 +0200

    Add the project

---
+++ src/main/java/nl/tudelft/pooralien/Launcher.java
@@ -0,0 +1,10 @@
+package main.java.nl.tudelft.pooralien;
+
+/**
+ * Created by mostafa on 7-9-17.
+ */
+public class Launcher {
+    public static void main(String[] args) {
+        System.out.println("Hi");
+    }
+}
