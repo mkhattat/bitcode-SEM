411e6ac3896738734553a442def798458a336a4c
Alex van der Linden <alexvanderlinden@me.com>
2017-09-09 22:33:29 +0200

    added MouseAction class that can recognize 3 mouse actions: click, drag horizontal, and drag vertical.

--- src/main/java/nl/tudelft/pooralien/EventHandler.java
+++ src/main/java/nl/tudelft/pooralien/EventHandler.java
@@ -28,7 +28,9 @@ public class EventHandler {
         }
     }
 
-    public class MouseHandler implements MouseListener, MouseMotionListener{
+    public class MouseHandler implements MouseListener, MouseMotionListener {
+
+        private MouseAction mouseAction;
 
         public MouseHandler(Launcher window) {
             window.addMouseListener(this);
@@ -36,8 +38,8 @@ public class EventHandler {
         }
 
         public void mousePressed(MouseEvent e) {
-            saySomething("Mouse pressed; # of clicks: "
-                    + e.getClickCount(), e);
+            System.out.println("Create MouseAction Object");
+            mouseAction = new MouseAction(e.getX(), e.getY());
         }
 
         public void mouseReleased(MouseEvent e) {
@@ -70,10 +72,63 @@ public class EventHandler {
         }
 
         public void mouseDragged(MouseEvent e) {
-            System.out.println("X : " + e.getX());
-            System.out.println("Y : " + e.getY());
+            mouseAction.setPosition(e.getX(), e.getY());
+            if (mouseAction.getMouseActionType() != MouseAction.CLICK_ACTION){
+
+                if (mouseAction.getMouseActionType() == MouseAction.HORIZONTAL_DRAG_ACTION) {
+                    System.out.println("Animate DRAG Horizontal x:" + e.getX() + ", y:" + e.getY());
+                } else if (mouseAction.getMouseActionType() == MouseAction.VERICAL_DRAG_ACTION) {
+                    System.out.println("Animate DRAG Vertical x:" + e.getX() + ", y:" + e.getY());
+                }
+            }
+        }
+
+        public class MouseAction {
+            public static final int CLICK_ACTION = 1;
+            public static final int HORIZONTAL_DRAG_ACTION = 2;
+            public static final int VERICAL_DRAG_ACTION = 3;
+
+            private static final int DRAG_TRIGGER_VALUE = 5;
+
+            private int xPosStart, yPosStart;
+            private int xPosCurrent, yPosCurrent;
+            public int mouseActionType;
+
+            public MouseAction(int xPos, int yPos) {
+                xPosStart = xPos;
+                yPosStart = yPos;
+                mouseActionType = CLICK_ACTION;
+            }
+
+            private void determineDragDirection(int xPos, int yPos) {
+                if (Math.abs(xPosStart - xPos) >= DRAG_TRIGGER_VALUE) {
+                    mouseActionType = HORIZONTAL_DRAG_ACTION;
+                } else if (Math.abs(yPosStart - yPos) >= DRAG_TRIGGER_VALUE) {
+                    mouseActionType = VERICAL_DRAG_ACTION;
+                } else {
+                    mouseActionType = CLICK_ACTION;
+                }
+            }
+
+            public void setPosition(int xPos, int yPos){
+                if (mouseActionType == CLICK_ACTION) {
+                    determineDragDirection(xPos, yPos);
+                }
+                xPosCurrent = xPos;
+                yPosCurrent = yPos;
+            }
+
+            public int[] getPosition(){
+                int[] positionArray = {xPosCurrent, yPosCurrent};
+                return positionArray;
+            }
+
+            public int getMouseActionType(){
+                return mouseActionType;
+            }
         }
 
     }
+
 
 }
