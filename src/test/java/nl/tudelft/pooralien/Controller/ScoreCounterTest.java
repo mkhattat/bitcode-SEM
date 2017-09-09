103528cdea753c09cd92e4b6ab787e13a26d0c49
IvoSchols <Ivoschols97@gmail.com>
2017-09-09 17:56:57 +0200

    - Added public in front of every method (somehow dissapeared?)

--- src/test/java/nl/tudelft/pooralien/Controller/ScoreCounterTest.java
+++ src/test/java/nl/tudelft/pooralien/Controller/ScoreCounterTest.java
@@ -14,47 +14,47 @@ public class ScoreCounterTest {
     ScoreCounter scoreCounter;
 
     @Before
-    void setup() {
+    public void setup() {
         scoreCounter = new ScoreCounter(0);
     }
 
     @Test
-    void negativeScoreUpdateNoChange() {
+    public void negativeScoreUpdateNoChange() {
         int currentScore = scoreCounter.getScore();
         scoreCounter.updateScore(-10);
         assertEquals(currentScore, scoreCounter.getScore());
     }
 
     @Test
-    void positiveScoreUpdateChange() {
+    public void positiveScoreUpdateChange() {
         int currentScore = scoreCounter.getScore() + (scoreCounter.getScorePerTile() * 10);
         scoreCounter.updateScore(10);
         assertEquals(currentScore, scoreCounter.getScore());
     }
 
     @Test
-    void negativeSetScoreNoChange() {
+    public void negativeSetScoreNoChange() {
         int currentScore = scoreCounter.getScore();
         scoreCounter.setScore(-1);
         assertEquals(currentScore, scoreCounter.getScore());
     }
 
     @Test
-    void positiveSetScoreToOne() {
+    public void positiveSetScoreToOne() {
         int currentScore = 1;
         scoreCounter.setScore(1);
         assertEquals(currentScore, scoreCounter.getScore());
     }
 
     @Test
-    void negativeSetScorePerTileNoChange() {
+    public void negativeSetScorePerTileNoChange() {
         int currentScorePerTile = scoreCounter.getScorePerTile();
         scoreCounter.setScorePerTile(-2);
         assertEquals(currentScorePerTile, scoreCounter.getScorePerTile());
     }
 
     @Test
-    void positiveSetScorePerTileToTwo() {
+    public void positiveSetScorePerTileToTwo() {
         int currentScorePerTile = 2;
         scoreCounter.setScorePerTile(2);
         assertEquals(currentScorePerTile, scoreCounter.getScorePerTile());
