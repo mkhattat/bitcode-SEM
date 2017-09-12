e029c61d106e3eb4a72ced99456ea868c92e8300
Mostafa Khattat <m.khattat@student.tudelft.nl>
2017-09-13 00:08:54 +0200

    Add Game class for controlling the flow of the game.

---
+++ src/main/java/nl/tudelft/pooralien/Controller/Game.java
@@ -0,0 +1,37 @@
+package nl.tudelft.pooralien.Controller;
+
+import nl.tudelft.pooralien.Board;
+
+/**
+ * class for controlling the flow of the game.
+ */
+public final class Game {
+    private static Game game;
+    private Board board;
+
+    /**
+     * Initialise the singleton Game object.
+     */
+    private Game() {
+        board = new Board();
+    }
+
+    /**
+     * Returns the single Game object.
+     * @return the Game object
+     */
+    public static synchronized Game getGame() {
+        if (game == null) {
+            game = new Game();
+        }
+        return game;
+    }
+
+    /**
+     * Returns the board.
+     * @return the board being used.
+     */
+    public Board getBoard() {
+        return board;
+    }
+}
