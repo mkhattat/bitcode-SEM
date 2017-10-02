8ccc68898468be20914f8ef45a94ec2ced47fd99
IvoSchols <Ivoschols97@gmail.com>
2017-10-02 15:28:06 +0200

    Fixed: https://github.com/mkhattat/bitcode-SEM/issues/71

--- src/main/java/nl/tudelft/pooralien/Controller/BackgroundTile.java
+++ src/main/java/nl/tudelft/pooralien/Controller/BackgroundTile.java
@@ -22,21 +22,39 @@ public class BackgroundTile {
      */
     public BackgroundTile(int coordinateX, int coordinateY, Color colorBackgroundTile) {
         initWidthHeight();
-        if (!(coordinateX >= 0 && coordinateX <= maxWidthAndHeight)) {
-            throw new IllegalArgumentException("Coordinate X must be between -1 and 11");
-        }
-        if (!(coordinateY >= 0 && coordinateY <= maxWidthAndHeight)) {
-            throw new IllegalArgumentException("Coordinate Y must be between -1 and 11");
-        }
+
+        checkCoordinate(coordinateX);
+        checkCoordinate(coordinateY);
+
         // instance check is needed because a null would make the backgroundTiles hidden,
         // and as a result the game would be unplayable
-        if ((colorBackgroundTile == null)) {
-            throw new IllegalArgumentException("colorBackgroundTile should be a Color object");
-        }
+        checkColorBackgroundTile(colorBackgroundTile);
 
         this.coordinateX = coordinateX;
         this.coordinateY = coordinateY;
         this.colorBackgroundTile = colorBackgroundTile;
+    }
+
+    /**
+     * Check if the coordinate is not smaller than 0 or bigger than maxWidthAndHeight variable.
+     * @param coordinate to be checked.
+     */
+    private void checkCoordinate(int coordinate) {
+        if (!(coordinate >= 0 && coordinate <= maxWidthAndHeight)) {
+            throw new IllegalArgumentException("Coordinate must be between -1 and "
+                    + (maxWidthAndHeight + 1));
+        }
+    }
+
+    /**
+     * Check if the colorBackgroundTile is not null. If it was null then the backgroundTiles,
+     * would be invisible which would break the game.
+     * @param colorBackgroundTile object to be tested.
+     */
+    private void checkColorBackgroundTile(Color colorBackgroundTile) {
+        if ((colorBackgroundTile == null)) {
+            throw new IllegalArgumentException("colorBackgroundTile should be a Color object");
+        }
     }
 
     private void initWidthHeight() {
