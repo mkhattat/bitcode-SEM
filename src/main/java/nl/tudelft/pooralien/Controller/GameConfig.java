141aaf4f52a0652fb000cb7fe49ef0f7b7e3212b
Alex van der Linden <alexvanderlinden@me.com>
2017-10-26 23:28:36 +0200

    Finished boundaries logic and added logging.

--- src/main/java/nl/tudelft/pooralien/Controller/GameConfig.java
+++ src/main/java/nl/tudelft/pooralien/Controller/GameConfig.java
@@ -46,6 +46,7 @@ public final class GameConfig {
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
+        System.out.println("Warning: exception in gameConfig.getBoolean. Using default value.");
         return standard;
     }
 
@@ -66,6 +67,7 @@ public final class GameConfig {
         try {
             List<Boolean> booleanList = cfg.getListBoolValueOf(name);
             if (booleanList.size() < minListLength | booleanList.size() > maxListLength) {
+                System.out.println("Warning: number of elements out of bounds in gameConfig.getBooleanList. Using default value.");
                 return standard;
             }
             else {
@@ -76,6 +78,7 @@ public final class GameConfig {
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
+        System.out.println("Warning: exception in gameConfig.getBooleanList. Using default value.");
         return standard;
     }
 
@@ -94,6 +97,7 @@ public final class GameConfig {
         try {
             Integer integer = cfg.getIntegerValueOf(name);
             if (integer < min | integer > max) {
+                System.out.println("Warning: value out of bounds in gameConfig.getInteger. Using default value.");
                 return standard;
             }
             else {
@@ -104,15 +108,16 @@ public final class GameConfig {
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
+        System.out.println("Warning: exception in gameConfig.getInteger. Using default value.");
         return standard;
     }
 
-    // TODO: Implement check boundaries logic
     /**
      * Retrieves an Integer List from the config.
      * Specifically, the Integer List corresponding
      * to the provided name.
      * @param name The name corresponding to the requested Integer List.
+     * @param minListLength The minimum number of elements in the Integer List.
      * @param maxListLength The maximum number of elements in the Integer List.
      * @param minValues The Integer List of minimum valid values.
      * @param maxValues The Integer List of maximum valid values.
@@ -120,19 +125,31 @@ public final class GameConfig {
      * @return The Integer List corresponding to the provided name.
      */
     @SuppressWarnings("unchecked")
-    public static List<Integer> getIntegerList(String name, Integer maxListLength, List<Integer> minValues,
-                                               List<Integer> maxValues, List<Integer> standard) {
+    public static List<Integer> getIntegerList(String name, Integer minListLength, Integer maxListLength,
+                                               List<Integer> minValues, List<Integer> maxValues,
+                                               List<Integer> standard) {
         try {
-            return (List<Integer>) cfg.getListIntValueOf(name);
+            List<Integer> integerList = cfg.getListIntValueOf(name);
+            if (integerList.size() < minListLength | integerList.size() > maxListLength) {
+                System.out.println("Warning: number of elements out of bounds in gameConfig.getIntegerList. Using default value.");
+                return standard;
+            }
+            for (int i = 0; i < integerList.size(); i++) {
+                if (integerList.get(i) < minValues.get(i) | integerList.get(i) > maxValues.get(i)) {
+                    System.out.println("Warning: value on index " + i + " out of bounds in gameConfig.getIntegerList. Using default value.");
+                    return standard;
+                }
+            }
+            return integerList;
         } catch (NotExistingVariableException e) {
             System.out.println("Integer List " + name + " not found.");
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
-        return null;
+        System.out.println("Warning: exception in gameConfig.getIntegerList. Using default value.");
+        return standard;
     }
 
-    // TODO: Implement check boundaries logic
     /**
      * Retrieves a Real (Double) value from the config.
      * Specifically, the Real value corresponding
@@ -145,21 +162,27 @@ public final class GameConfig {
      */
     public static Double getReal(String name, Double min, Double max, Double standard) {
         try {
-            return cfg.getRealValueOf(name);
+            Double real = cfg.getRealValueOf(name);
+            if (real < min | real > max) {
+                System.out.println("Warning: value out of bounds in gameConfig.getReal. Using default value.");
+                return standard;
+            }
+            return real;
         } catch (NotExistingVariableException e) {
             System.out.println("Real " + name + " not found.");
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
-        return null;
+        System.out.println("Warning: exception in gameConfig.getReal. Using default value.");
+        return standard;
     }
 
-    // TODO: Implement check boundaries logic
     /**
      * Retrieves a Real (Double) List from the config.
      * Specifically, the Real List corresponding
      * to the provided name.
      * @param name The name corresponding to the requested Real List.
+     * @param minListLength The minimum number of elements in the Double List.
      * @param maxListLength The maximum number of elements in the Double List.
      * @param minValues The Double List of minimum valid values.
      * @param maxValues The Double List of maximum valid values.
@@ -167,62 +190,121 @@ public final class GameConfig {
      * @return The Real List corresponding to the provided name.
      */
     @SuppressWarnings("unchecked")
-    public static List<Double> getRealList(String name, int maxListLength, List<Double> minValues,
-                                           List<Double> maxValues, List<Double> standard) {
+    public static List<Double> getRealList(String name, Integer minListLength, Integer maxListLength,
+                                           List<Double> minValues, List<Double> maxValues,
+                                           List<Double> standard) {
         try {
-            return (List<Double>) cfg.getListRealValueOf(name);
+            List<Double> doubleList = cfg.getListRealValueOf(name);
+            if (doubleList.size() < minListLength | doubleList.size() > maxListLength) {
+                System.out.println("Warning: number of elements out of bounds in gameConfig.getRealList. Using default value.");
+                return standard;
+            }
+            for (int i = 0; i < doubleList.size(); i++) {
+                if (doubleList.get(i) < minValues.get(i) | doubleList.get(i) > maxValues.get(i)) {
+                    System.out.println("Warning: value on index " + i + " out of bounds in gameConfig.getRealList. Using default value.");
+                    return standard;
+                }
+            }
+            return doubleList;
         } catch (NotExistingVariableException e) {
             System.out.println("Real List " + name + " not found.");
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
-        return null;
+        System.out.println("Warning: exception in gameConfig.getRealList. Using default value.");
+        return standard;
     }
 
-    // TODO: Implement check boundaries logic
     /**
      * Retrieves a String value from the config.
      * Specifically, the String value corresponding
      * to the provided name.
      * @param name The name corresponding to the requested String.
-     * @param maxLength The maximum number of characters in the String
+     * @param minLength The minimum number of characters in the String.
+     * @param maxLength The maximum number of characters in the String.
      * @param standard The default String.
      * @return The String corresponding to the provided name.
      */
-    public static String getString(String name, int maxLength, String standard) {
+    public static String getString(String name, Integer minLength, Integer maxLength, String standard) {
         try {
-            return cfg.getStringValueOf(name);
+            String string = cfg.getStringValueOf(name);
+            if (string.length() < minLength | string.length() > maxLength) {
+                System.out.println("Warning: number of characters in string out of bounds in gameConfig.getString. Using default value.");
+                return standard;
+            }
+            if (!isAlpha(string)) {
+                System.out.println("Warning: string is not alphabetic in gameConfig.getString. Using default value.");
+                return standard;
+            }
+            return string;
         } catch (NotExistingVariableException e) {
             System.out.println("String " + name + " not found.");
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
-        return null;
+        System.out.println("Warning: exception in gameConfig.getString. Using default value.");
+        return standard;
     }
 
-    // TODO: Implement check boundaries logic
     /**
      * Retrieves a String List from the config.
      * Specifically, the String List corresponding
      * to the provided name.
      * @param name The name corresponding to the requested String List.
+     * @param minListLength The minimum number of elements in the Integer List.
      * @param maxListLength The maximum number of elements in the Integer List.
+     * @param minStringLengths The ordered list of the minimum number of characters in
+     *                         the String from the ordered String List.
      * @param maxStringLengths The ordered list of the maximum number of characters in
      *                         the String from the ordered String List.
      * @param standard The default String List.
      * @return The String List corresponding to the provided name.
      */
     @SuppressWarnings("unchecked")
-    public static List<String> getStringList(String name, int maxListLength, List<Integer> maxStringLengths,
-                                             String standard) {
+    public static List<String> getStringList(String name, Integer minListLength, Integer maxListLength,
+                                             List<Integer> minStringLengths, List<Integer> maxStringLengths,
+                                             List<String> standard) {
         try {
-            return (List<String>) cfg.getListStringValueOf(name);
+            List<String> stringList = cfg.getListStringValueOf(name);
+            if (stringList.size() < minListLength | stringList.size() > maxListLength) {
+                System.out.println("Warning: number of elements out of bounds in gameConfig.getStringList. Using default value.");
+                return standard;
+            }
+            for (int i = 0; i < stringList.size(); i++) {
+                if ((stringList.get(i).length() < minStringLengths.get(i))
+                        | (stringList.get(i).length() > maxStringLengths.get(i))) {
+                    System.out.println("Warning: string length on index " + i + " out of bounds in gameConfig.getRealList. Using default value.");
+                    return standard;
+                }
+                if (!isAlpha(stringList.get(i))) {
+                    System.out.println("Warning: string on index " + i + " is not alphabetic in gameConfig.getRealList. Using default value.");
+                    return standard;
+                }
+            }
+            return stringList;
         } catch (NotExistingVariableException e) {
             System.out.println("String List " + name + " not found.");
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
-        return null;
+        System.out.println("Warning: exception in gameConfig.getStringList. Using default value.");
+        return standard;
+    }
+
+    /**
+     * Checks if a given string is alphabetic to
+     * prevent code injection.
+     * @param string The string to be checked.
+     * @return True if string is alphabetic, else False.
+     */
+    private static boolean isAlpha(String string) {
+        char[] chars = string.toCharArray();
+        for (char c : chars) {
+            if (!Character.isLetter(c)) {
+                return false;
+            }
+        }
+        return true;
     }
 
     /**
