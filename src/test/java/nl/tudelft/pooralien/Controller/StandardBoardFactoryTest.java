5b3cc50251ba67f0407bfd9faa05b59696d972d0
DexterQvL <dq_vanleeuwen@hotmail.com>
2017-11-01 12:43:21 +0100

    optimized imports

--- src/test/java/nl/tudelft/pooralien/Controller/StandardBoardFactoryTest.java
+++ src/test/java/nl/tudelft/pooralien/Controller/StandardBoardFactoryTest.java
@@ -3,7 +3,9 @@ package nl.tudelft.pooralien.Controller;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class StandardBoardFactoryTest {
     private BoardFactory bFactory;
