4e7cb8b6882748ca5b45fce193d482d95013e842
DexterQvL <dq_vanleeuwen@hotmail.com>
2017-11-02 12:05:19 +0100

    added hardboard and hardboardfactory classes for hard mode

---
+++ src/main/java/nl/tudelft/pooralien/Controller/HardBoard.java
@@ -0,0 +1,61 @@
+package nl.tudelft.pooralien.Controller;
+
+import nl.tu.delft.defpro.exception.NotExistingVariableException;
+import nl.tudelft.item.StandardItemFactory;
+import nl.tudelft.pooralien.Launcher;
+
+/**
+ * Board for hard mode.
+ */
+public class HardBoard extends Board {
+
+    /**
+     * Initializes the width of the board using the config file.
+     * @return The initial width of the board from the config file.
+     */
+    protected int initWidth() {
+        try {
+            return Launcher.getGameCfg().getIntegerValueOf("maxBoardWidth");
+        } catch (NotExistingVariableException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+        return -1;
+    }
+
+    /**
+     * Initializes the height of the board using the config file.
+     * @return The initial height of the board from the config file.
+     */
+    protected int initHeight() {
+        try {
+            return Launcher.getGameCfg().getIntegerValueOf("maxBoardHeight");
+        } catch (NotExistingVariableException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+        return -1;
+    }
+
+    /**
+     * Initializes the minimum group size using the config file.
+     * @return The minimum group size from the config file.
+     */
+    protected int initMinGroupSize() {
+        try {
+            return Launcher.getGameCfg().getIntegerValueOf("minItemsInRow");
+        } catch (NotExistingVariableException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+        return -1;
+    }
+
+    /**
+     * Initializes the ItemFactory.
+     * @return The initial ItemFactory.
+     */
+    protected StandardItemFactory initItemFactory() {
+        return new StandardItemFactory();
+    }
+}
