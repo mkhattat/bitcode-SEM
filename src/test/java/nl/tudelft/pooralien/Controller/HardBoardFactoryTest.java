c093f8dcc3ebff675fa0761ae20254de439144de
DexterQvL <dq_vanleeuwen@hotmail.com>
2017-11-03 11:04:57 +0100

    Added test for hardboardfactory

---
+++ src/test/java/nl/tudelft/pooralien/Controller/HardBoardFactoryTest.java
@@ -0,0 +1,105 @@
+package nl.tudelft.pooralien.Controller;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class HardBoardFactoryTest {
+    private BoardFactory bFactory;
+
+    @Before
+    public void setUp() throws Exception {
+        bFactory = new HardBoardFactory();
+    }
+
+    @Test
+    public void createBoard() throws Exception {
+        String layout = "AXE BONE EYE LEAF MASK MOUTH SUN AXE BONE EYE "
+                + "LEAF MASK MOUTH SUN AXE BONE EYE LEAF MASK MOUTH "
+                + "SUN AXE BONE EYE LEAF MASK MOUTH SUN AXE BONE "
+                + "EYE LEAF MASK MOUTH SUN AXE BONE EYE LEAF MASK "
+                + "MOUTH SUN AXE BONE EYE LEAF MASK MOUTH SUN AXE "
+                + "BONE EYE LEAF MASK MOUTH SUN AXE BONE EYE LEAF "
+                + "MASK MOUTH SUN AXE BONE EYE LEAF MASK MOUTH SUN "
+                + "AXE BONE EYE LEAF MASK MOUTH SUN AXE BONE EYE "
+                + "LEAF MASK MOUTH SUN AXE BONE EYE LEAF MASK MOUTH "
+                + "SUN AXE BONE EYE LEAF MASK MOUTH SUN AXE BONE ";
+        Board board = bFactory.createBoard(layout);
+
+        assertTrue(board instanceof HardBoard);
+        assertEquals(layout, board.toString());
+    }
+
+    @Test
+    public void createBoardNotEnoughItems() throws Exception {
+        try {
+            bFactory.createBoard("Axe Eye ");
+        } catch (IllegalArgumentException e) {
+            assertEquals(
+                    "The provided board layout contained "
+                            + 2 + " items, but should contain "
+                            + 100 + " items.",
+                    e.getMessage()
+            );
+        }
+    }
+
+    @Test
+    public void createBoardTooManyItems() throws Exception {
+        try {
+            bFactory.createBoard("Axe Eye Bone Mask Bone Bone Sun Mouth Mouth Leaf "
+                    + "Axe Eye Bone Mask Bone Bone Sun Mouth Mouth Leaf "
+                    + "Axe Eye Bone Mask Bone Bone Sun Mouth Mouth Leaf "
+                    + "Axe Eye Bone Mask Bone Bone Sun Mouth Mouth Leaf "
+                    + "Axe Eye Bone Mask Bone Bone Sun Mouth Mouth Leaf "
+                    + "Axe Eye Bone Mask Bone Bone Sun Mouth Mouth Leaf "
+                    + "Axe Eye Bone Mask Bone Bone Sun Mouth Mouth Leaf "
+                    + "Axe Eye Bone Mask Bone Bone Sun Mouth Mouth Leaf "
+                    + "Axe Eye Bone Mask Bone Bone Sun Mouth Mouth Leaf "
+                    + "Axe Eye Bone Mask Bone Bone Sun Mouth Mouth Leaf "
+                    + "Leaf ");
+        } catch (IllegalArgumentException e) {
+            assertEquals(
+                    "The provided board layout contained "
+                            + 101 + " items, but should contain "
+                            + 100 + " items.",
+                    e.getMessage()
+            );
+        }
+    }
+
+    @Test
+    public void createBoardIllegalItem() throws Exception {
+        try {
+            bFactory.createBoard("Axe Eye Bone Mask Bone Bone Sun Mouth Mouth Leaf "
+                    + "Axe Eye Bone Mask Bone Bone Sun Mouth Mouth Leaf "
+                    + "Axe Eye Bone Mask Bone Bone Sun Mouth Mouth Leaf "
+                    + "Axe Eye Bone Mask Bone Bone Sun Mouth Mouth Leaf "
+                    + "Axe Eye Bone Mask Bone Bone Sun Mouth Mouth Leaf "
+                    + "Axe Eye Bone Mask Bone Bone Sun Mouth Mouth Leaf "
+                    + "Axe Eye Bone Mask Bone Bone Sun Mouth Mouth Leaf "
+                    + "Axe Eye Bone Mask Bone Bone Sun Mouth Mouth Leaf "
+                    + "Axe Eye Bone Mask Bone Bone Sun Mouth Mouth Leaf "
+                    + "Axe Eye Bone Mask Bone Bone Sun Mouth Mouth Moon "
+            );
+        } catch (IllegalArgumentException e) {
+            assertEquals(
+                    "The provided item name: Moon"
+                            + "\nDoes not match any of the following: "
+                            + "axe, bone, eye, leaf, mask, mouth or sun.",
+                    e.getMessage()
+            );
+        }
+    }
+
+    @Test
+    public void createRandomBoard() throws Exception {
+        Board board = bFactory.createRandomBoard();
+        assertTrue(board instanceof HardBoard);
+        assertFalse(board.removeGroups());
+    }
+
+}
